a5abad28e17615ebffdf99d0eff798a3848ec194
Dave Goddard <dave@goddard.id.au>
2018-11-06 11:06:35 -0800

    wip

--- src/crystal/finalize/finalize.go
+++ src/crystal/finalize/finalize.go
@@ -1,48 +1,41 @@
 package finalize
 
 import (
-	"io"
+	"fmt"
 
 	"github.com/cloudfoundry/libbuildpack"
 )
 
 type Stager interface {
-	//TODO: See more options at https://github.com/cloudfoundry/libbuildpack/blob/master/stager.go
 	BuildDir() string
 	DepDir() string
 	DepsIdx() string
 	DepsDir() string
 }
 
-type Manifest interface {
-	//TODO: See more options at https://github.com/cloudfoundry/libbuildpack/blob/master/manifest.go
-	AllDependencyVersions(string) []string
-}
-
-type Installer interface {
-	//TODO: See more options at https://github.com/cloudfoundry/libbuildpack/blob/master/installer.go
-	DefaultVersion(string) (libbuildpack.Dependency, error)
-	InstallDependency(libbuildpack.Dependency, string) error
-	InstallOnlyVersion(string, string) error
-}
-
-type Command interface {
-	//TODO: See more options at https://github.com/cloudfoundry/libbuildpack/blob/master/command.go
-	Execute(string, io.Writer, io.Writer, string, ...string) error
-	Output(dir string, program string, args ...string) (string, error)
-}
-
 type Finalizer struct {
-	Manifest Manifest
-	Stager   Stager
-	Command  Command
-	Log      *libbuildpack.Logger
+	Stager Stager
+	Log    *libbuildpack.Logger
 }
 
 func (f *Finalizer) Run() error {
 	f.Log.BeginStep("Configuring crystal")
 
-	// TODO: Prepare app for launch here here...
+	data, err := f.GenerateReleaseYaml()
+	if err != nil {
+		f.Log.Error("Error generating release YAML: %v", err)
+		return err
+	}
+	libbuildpack.NewYAML().Write("/tmp/crystal-buildpack-release-step.yml", data)
 
 	return nil
+}
+
+func (f *Finalizer) GenerateReleaseYaml() (map[string]map[string]string, error) {
+	processTypes := map[string]string{
+		"web": fmt.Sprintf("$DEPS_DIR/%s/app --port $PORT", f.Stager.DepsIdx()),
+	}
+	return map[string]map[string]string{
+		"default_process_types": processTypes,
+	}, nil
 }
--- src/crystal/integration/integration_suite_test.go
+++ src/crystal/integration/integration_suite_test.go
@@ -3,14 +3,11 @@ package integration_test
 import (
 	"encoding/json"
 	"flag"
-	"fmt"
-	"net/url"
-	"os"
-	"os/exec"
-	"path/filepath"
+	"io/ioutil"
+	"strings"
 	"time"
 
-	"github.com/blang/semver"
+	"github.com/cloudfoundry/libbuildpack"
 	"github.com/cloudfoundry/libbuildpack/cutlass"
 
 	. "github.com/onsi/ginkgo"
@@ -34,7 +31,7 @@ func init() {
 var _ = SynchronizedBeforeSuite(func() []byte {
 	// Run once
 	if buildpackVersion == "" {
-		packagedBuildpack, err := cutlass.PackageUniquelyVersionedBuildpack("") // "" denotes any stack. Use specific stack (e.g. "cflinuxfs2" if desired)
+		packagedBuildpack, err := cutlass.PackageUniquelyVersionedBuildpack()
 		Expect(err).NotTo(HaveOccurred())
 
 		data, err := json.Marshal(packagedBuildpack)
@@ -79,86 +76,14 @@ func PushAppAndConfirm(app *cutlass.App) {
 	Expect(app.ConfirmBuildpack(buildpackVersion)).To(Succeed())
 }
 
-func Restart(app *cutlass.App) {
-	Expect(app.Restart()).To(Succeed())
-	Eventually(func() ([]string, error) { return app.InstanceStates() }, 20*time.Second).Should(Equal([]string{"RUNNING"}))
+func log(app *cutlass.App) string {
+	var cleaner = strings.NewReplacer("\033[31;1m", "", "\033[33;1m", "", "\033[34;1m", "", "\033[0m", "", "**WARNING**", "WARNING:", "**ERROR**", "ERROR:")
+	return cleaner.Replace(app.Stdout.String())
 }
 
-func ApiGreaterThan(version string) bool {
-	apiVersionString, err := cutlass.ApiVersion()
-	Expect(err).To(BeNil())
-	apiVersion, err := semver.Make(apiVersionString)
-	Expect(err).To(BeNil())
-	reqVersion, err := semver.ParseRange(">= " + version)
-	Expect(err).To(BeNil())
-	return reqVersion(apiVersion)
-}
-
-func ApiHasTask() bool {
-	return ApiGreaterThan("2.75.0")
-}
-func ApiHasMultiBuildpack() bool {
-	return ApiGreaterThan("2.90.0")
-}
-
-func AssertUsesProxyDuringStagingIfPresent(fixtureName string) {
-	Context("with an uncached buildpack", func() {
-		BeforeEach(func() {
-			if cutlass.Cached {
-				Skip("Running cached tests")
-			}
-		})
-
-		It("uses a proxy during staging if present", func() {
-			proxy, err := cutlass.NewProxy()
-			Expect(err).To(BeNil())
-			defer proxy.Close()
-
-			bpFile := filepath.Join(bpDir, buildpackVersion+"tmp")
-			cmd := exec.Command("cp", packagedBuildpack.File, bpFile)
-			err = cmd.Run()
-			Expect(err).To(BeNil())
-			defer os.Remove(bpFile)
-
-			traffic, built, err := cutlass.InternetTraffic(
-				bpDir,
-				filepath.Join("fixtures", fixtureName),
-				bpFile,
-				[]string{"HTTP_PROXY=" + proxy.URL, "HTTPS_PROXY=" + proxy.URL},
-			)
-			Expect(err).To(BeNil())
-			Expect(built).To(BeTrue())
-
-			destUrl, err := url.Parse(proxy.URL)
-			Expect(err).To(BeNil())
-
-			Expect(cutlass.UniqueDestination(
-				traffic, fmt.Sprintf("%s.%s", destUrl.Hostname(), destUrl.Port()),
-			)).To(BeNil())
-		})
-	})
-}
-
-func AssertNoInternetTraffic(fixtureName string) {
-	It("has no traffic", func() {
-		if !cutlass.Cached {
-			Skip("Running uncached tests")
-		}
-
-		bpFile := filepath.Join(bpDir, buildpackVersion+"tmp")
-		cmd := exec.Command("cp", packagedBuildpack.File, bpFile)
-		err := cmd.Run()
-		Expect(err).To(BeNil())
-		defer os.Remove(bpFile)
-
-		traffic, built, err := cutlass.InternetTraffic(
-			bpDir,
-			filepath.Join("fixtures", fixtureName),
-			bpFile,
-			[]string{},
-		)
-		Expect(err).To(BeNil())
-		Expect(built).To(BeTrue())
-		Expect(traffic).To(BeEmpty())
-	})
+func CopyFixture(oldPath string) string {
+	newPath, err := ioutil.TempDir("", "fixture.")
+	Expect(err).NotTo(HaveOccurred())
+	Expect(libbuildpack.CopyDirectory(oldPath, newPath)).To(Succeed())
+	return newPath
 }
